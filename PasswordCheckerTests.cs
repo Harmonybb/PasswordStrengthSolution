194db3b3ac3cba691fadecf0e5558b62e8c7ed20
Harmony Brown <Harmony.Brown24@yahoo.com>
2025-10-17 01:42:15 -0400

    Initial commit - password strength solution with tests

---
+++ PasswordCheckerTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using PasswordLibrary;
+
+namespace PasswordLibrary.Tests
+{
+    [TestFixture]
+    public class PasswordCheckerTests
+    {
+        [TestCase("", ExpectedResult = PasswordChecker.Strength.INELIGIBLE)]
+        [TestCase("123456", ExpectedResult = PasswordChecker.Strength.WEAK)]
+        [TestCase("abcdef", ExpectedResult = PasswordChecker.Strength.WEAK)]
+        [TestCase("ABCDEF", ExpectedResult = PasswordChecker.Strength.WEAK)]
+        [TestCase("abc123", ExpectedResult = PasswordChecker.Strength.MEDIUM)]
+        [TestCase("abc$123", ExpectedResult = PasswordChecker.Strength.MEDIUM)]
+        [TestCase("Abc123", ExpectedResult = PasswordChecker.Strength.MEDIUM)]
+        [TestCase("Abc123$", ExpectedResult = PasswordChecker.Strength.STRONG)]
+        [TestCase("!!!", ExpectedResult = PasswordChecker.Strength.WEAK)]
+        public PasswordChecker.Strength TestPasswordStrength(string password)
+        {
+            return PasswordChecker.CheckStrength(password);
+        }
+    }
+}
+
---
+++ PasswordLibrary/PasswordChecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace PasswordLibrary
+{
+    public class PasswordChecker
+    {
+        public enum Strength
+        {
+            INELIGIBLE,
+            WEAK,
+            MEDIUM,
+            STRONG
+        }
+
+        public static Strength CheckStrength(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return Strength.INELIGIBLE;
+
+            int criteriaMet = 0;
+
+            if (password.Any(char.IsUpper)) criteriaMet++;
+            if (password.Any(char.IsLower)) criteriaMet++;
+            if (password.Any(char.IsDigit)) criteriaMet++;
+            if (password.Any(ch => !char.IsLetterOrDigit(ch))) criteriaMet++;
+
+            return criteriaMet switch
+            {
+                0 => Strength.INELIGIBLE,
+                1 => Strength.WEAK,
+                2 or 3 => Strength.MEDIUM,
+                4 => Strength.STRONG,
+                _ => Strength.INELIGIBLE
+            };
+        }
+    }
+}
+
